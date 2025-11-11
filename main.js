a0f4002a84e61581176126e769b8f634ed8f8297
CoolUBG <139328026+CoolUBG@users.noreply.github.com>
2025-11-11 18:38:03 +1100

    remove halloween theme

--- main.js
+++ main.js
@@ -497,12 +497,12 @@ function fullscreenFunction2() {
 
 function applyStoredSettings() {
     // Preset values
-    //const presetBackgroundImage = '/images/backgrounds/background-blue-old.png';
-    const presetBackgroundImage = '/images/backgrounds/halloween-2025.jpg';
-    //const presetPrimaryColor = '#111E2C';
-    const presetPrimaryColor = '#FA5500';
-    //const presetSecondaryColor = '#58AAFC';
-    const presetSecondaryColor = '#000000';
+    const presetBackgroundImage = '/images/backgrounds/background-blue-old.png';
+    //const presetBackgroundImage = '/images/backgrounds/halloween-2025.jpg';
+    const presetPrimaryColor = '#111E2C';
+    //const presetPrimaryColor = '#FA5500';
+    const presetSecondaryColor = '#58AAFC';
+    //const presetSecondaryColor = '#000000';
     const presetBackgroundRes = '100';
 
     // Get the "customisation" data from localStorage
